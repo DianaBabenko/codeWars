1c7c26595f77ba6d0690365cd213f3087478bf5e
DianaBabenko <baben.k.o.diana2017@gmail.com>
2019-10-24 23:40:00 +0300

    add task with 8 kyu

--- script.js
+++ script.js
@@ -13,3 +13,9 @@ function solution(str){
 function removeChar(str) {
     return str.slice(1, -1);
 }
+//Be Concise I - The Ternary Operator
+
+function describeAge(age) {
+    return "You're a(n) "
+        + (age < 13 ? "kid" : age < 18 ? "teenager" : age < 65 ? "adult" : "elderly")
+}
